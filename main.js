eb4baf372c2c36ea226941f40271ee826d63155b
BenScott <benscott92@gmail.com>
2020-06-18 14:30:46 -0400

    project init

---
+++ main.js
@@ -0,0 +1,76 @@
+// Rules :For these katas, instead of using console.log(), document.write(), or someElement.innerHTML, you are required to insert your results into the HTML using the document.createElement() and node.append() methods.
+
+let kataList = document.createElement('div')
+document.body.append(kataList)
+kataList.append('Im here')
+
+
+// Kata 1 Display the numbers from 1 to 20. (1, 2, 3, …, 19, 20)
+
+
+
+// Kata 2 Display the even numbers from 1 to 20. (2, 4, 6, …, 18, 20)
+
+
+
+// Kata 3 Display the odd numbers from 1 to 20. (1, 3, 5, …, 17, 19)
+
+
+
+// Kata 4 Display the multiples of 5 up to 100. (5, 10, 15, …, 95, 100)
+
+
+
+// Kata 5 Display the square numbers from 1 up to 100. (1, 4, 9, …, 81, 100)
+
+
+
+// Kata 6 Display the numbers counting backwards from 20 to 1. (20, 19, 18, …, 2, 1)
+
+
+
+// Kata 7 Display the even numbers counting backwards from 20 to 1. (20, 18, 16, …, 4, 2)
+
+
+
+// Kata 8 Display the odd numbers from 20 to 1, counting backwards. (19, 17, 15, …, 3, 1)
+
+
+
+// Kata 9 Display the multiples of 5, counting down from 100 to 1. (100, 95, 90, …, 10, 5)
+
+
+
+// Kata 10 Display the square numbers, counting down from 100. (100, 81, 64, …, 4, 1)
+
+
+
+// Kata 11 Display the 20 elements of sampleArray. (469, 755, 244, …, 940, 472)
+
+
+
+// Kata 12 Display all the even numbers contained in sampleArray. (244, 758, 450, …, 940, 472)
+
+
+
+// Kata 13 Display all the odd numbers contained in sampleArray. (469, 755, 245, …, 179, 535)
+
+
+
+// Kata 14 Display the square of each element in sampleArray. (219961, 570025, …, 222784)
+
+
+
+// Kata 15 Display the sum of all the numbers from 1 to 20.
+
+
+
+// Kata 16 Display the sum of all the elements in sampleArray.
+
+
+
+// Kata 17Display the smallest element in sampleArray.
+
+
+
+// Kata 18 Display the largest element in sampleArray.
